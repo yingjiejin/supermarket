2edbf2c67b671a523e2a0f7fe056f27e452cae14
jinyingjie <396146201@qq.com>
2017-04-20 20:41:35 +0800

    添加json工具类

---
+++ super-market-utils/src/main/java/utils/json/JsonUtils.java
@@ -0,0 +1,52 @@
+package utils.json;
+
+import com.jayway.jsonpath.JsonPath;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JsonUtils {
+
+	private static String appData = "";
+
+	static {
+
+		BufferedReader reader = null;
+		try {
+			PathMatchingResourcePatternResolver patternResolver = new PathMatchingResourcePatternResolver();
+			Resource[] resources = patternResolver.getResources("classpath*:appData.json");
+			if (resources != null && resources.length > 0) {
+				reader = new BufferedReader(new InputStreamReader(resources[0].getInputStream(), "UTF-8"));
+				String temp = null;
+				while ((temp = reader.readLine()) != null) {
+					appData += temp;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static boolean isValidJsonPathForAppData(String jsonPath) {
+		try {
+			Object o = JsonPath.read(appData, jsonPath);
+			return o != null;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
+}
